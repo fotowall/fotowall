dbd013f0ddfe9cf7ae72a96179a0dc6033b44434
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-11-13 18:00:50 +0100

    [bugfix] Random number compatibility and wordcloud generation

--- Shared/Compat.h
+++ Shared/Compat.h
@@ -2,14 +2,16 @@
 #include <QtGlobal>
 
 #if QT_VERSION >= QT_VERSION_CHECK(5, 1, 0)
-#  include <QRandomGenerator>
+#  include <cstdlib>
 #endif
 
 namespace compat
 {
 #if QT_VERSION >= QT_VERSION_CHECK(5, 1, 0)
 inline int qrand()
-{ return QRandomGenerator::global()->generate(); }
+{
+  return std::rand();
+}
 #else
 inline int qrand()
 { return ::qrand(); }
